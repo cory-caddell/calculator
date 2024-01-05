e290dc9d3b355525c8c029280e1750988903b841
cdc-001 <cory.caddell@gmail.com>
2024-01-05 17:03:44 -0600

    add buttons

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let num1;
 let operator;
 let num2;
 
+createCalcButtons();
+
+
+//////////// arithmetic functions ///////////////
 function add(a, b) {
     return a + b;
 }
@@ -34,4 +38,24 @@ function operate(num1, operator, num2) {
             return divide(num1, num2);
             break;
     }
+}
+
+/////////// calculator div functions /////////////////
+
+function createCalcButtons() {
+    const container = document.querySelector('.calc-container');
+
+    for (let i = 0; i < 10; i++) {
+        const digits = document.createElement('button');
+        digits.textContent = i;
+        container.appendChild(digits);
+    };
+
+    const operands = ['/', '*', '-', '=', '<-']; 
+
+    operands.forEach((item) => {
+        const operand = document.createElement('button');
+        operand.textContent = item;
+        container.appendChild(operand);
+    });
 }
